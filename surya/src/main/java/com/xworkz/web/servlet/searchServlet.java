7a651ed472f8d11758347e9725782952d743cdf3
surya5151 <surya2021.xworkz@gmail.com>
2021-10-26 13:55:12 +0530

    object creation

---
+++ surya/src/main/java/com/xworkz/web/servlet/searchServlet.java
@@ -0,0 +1,13 @@
+package com.xworkz.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class searchServlet extends HttpServlet {
+	
+	public	searchServlet(){
+		
+		System.out.println("Object created "+this.getClass().getSimpleName());
+			
+	}
+
+}
